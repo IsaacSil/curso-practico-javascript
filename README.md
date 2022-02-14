451906779c999772e45dccc20b45250bea7a6727
Isaac Silupú <isaacsilupu@outlook.com>
2022-02-13 21:41:10 -0500

    update courses

--- README.md
+++ README.md
@@ -16,3 +16,17 @@
 - Segundo paso: implementar la fórmulas en JS
 - Tercer paso: crear funciones
 - Cuarto paso: integrar JS con HTML
+
+## Taller #3: porcentajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar la fórmulas en JS
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Taller #4: Diferencia Salarial
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar la fórmulas en JS
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
--- descuentos.js
+++ descuentos.js
@@ -3,27 +3,97 @@ const discountPrice = (price, discount) => {
     return newPrice;
 }
 
-const findCoupon = (coupon) => {
-    switch (coupon) {
-        case '10%': return 10;
-        case '20%': return 20;
-        default: return 0;
-    };
-};
+//Solución con arrays de objetos:
+const coupons = [
+    {name: 'ten', discount: 10,},
+    {name: 'twelve', discount: 20,},
+    {name: 'thirty', discount: 30,},
+];
 
 const calculateDiscountPrice = () => {
     let inputPrice = document.getElementById('inputPrice');
-    let inputDiscount = document.getElementById('inputDiscount');
     let inputCoupon = document.getElementById('inputCoupon');
+    let priceValue = inputPrice.value;
+    let couponValue = inputCoupon.value;
+/*     const isCouponValid = (coupon) => {
+        return coupon.name === couponValue;
+    }; */
 
-    let calculatedPrice = discountPrice(inputPrice.value, inputDiscount.value);
-    
-    let couponValue = findCoupon(inputCoupon.value);
-    if (couponValue != 0) {
-        let couponPrice = discountPrice(calculatedPrice, couponValue);
-        resultP.innerText = `Cupón válido. El precio con descuento y cupón incluido es: ${couponPrice}`;
+    const userCoupon = coupons.find( (coupon) => {
+        return coupon.name === couponValue;
+    });
+
+    if (!userCoupon) {
+        resultP.innerText = `El cupón ${couponValue} no es válido`;
     } else {
-        resultP.innerText = `Cupón inválido. El precio con descuento es: ${calculatedPrice}`;
-    };
+        let discount = userCoupon.discount;
+        let couponPrice = discountPrice(priceValue, discount);
+        resultP.innerText = `Cupón válido. El precio con cupón es: ${couponPrice}`;
+    }
+
 };
+
+
+
+/* 
+const calculateDiscountPrice = () => {
+    let inputPrice = document.getElementById('inputPrice');
+    let inputCoupon = document.getElementById('inputCoupon');
+   
+    let couponValue = findCoupon(coupons, inputCoupon.value);
+    if (couponValue != 0) {
+        let couponPrice = discountPrice(inputPrice.value, couponValue);
+        resultP.innerText = `Cupón válido. El precio con cupón es: ${couponPrice}`;
+    } else {
+        resultP.innerText = `El cupón ${inputCoupon.value} es inválido`;
+    };
+}; */
+
+
+
+//Solución solo con arrays:
+/* const coupons = [
+    'ten', 
+    'twelve', 
+    'thirty',
+    'forty'
+];
+
+const findCoupon = (coupons, value) => {
+    let discount = 0;
+    switch (value) {
+        case coupons[0]: discount = 10;
+        break;
+        case coupons[1]: discount = 20;
+        break;
+        case coupons[2]: discount = 30;
+        break;
+        default: discount = 0;
+    };
+    return discount;
+}; */
+
+/* const findCoupon = (coupons, coupon) => {
+    switch (coupon) {
+        case coupons[0]: return 10;
+        case coupons[1]: return 20;
+        case coupons[2]: return 30;
+        case coupons[3]: return 40;
+        default: return 0;
+    };
+}; */
+
+
+/* const calculateDiscountPrice = () => {
+    let inputPrice = document.getElementById('inputPrice');
+    let inputCoupon = document.getElementById('inputCoupon');
+   
+    let couponValue = findCoupon(coupons, inputCoupon.value);
+    if (couponValue != 0) {
+        let couponPrice = discountPrice(inputPrice.value, couponValue);
+        resultP.innerText = `Cupón válido. El precio con cupón es: ${couponPrice}`;
+    } else {
+        resultP.innerText = `El cupón ${inputCoupon.value} es inválido`;
+    };
+}; */
 
